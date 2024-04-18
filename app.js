2a23266109585bc8e49bdfe26c2362b06d7e641b
Faizantechy <faizanraza112112@gmail.com>
2024-04-18 19:07:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector(".btn");
+var fs = document.querySelector("#fs");
+var modal = document.querySelector("#modal");
+
+btn.addEventListener("click", function () {
+  fs.style.opacity = "1";
+  modal.style.top = "15%";
+});
